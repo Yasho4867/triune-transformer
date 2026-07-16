aea6605ecb928b3d810bb574253a5b90b9776029
Yash Bharadwaj <yashbharadwaj456@gmail.com>
2026-07-16 18:40:28 +0000

    Implement Trainer interface

--- triune/trainer/trainer.py
+++ triune/trainer/trainer.py
@@ -1,13 +1,30 @@
-"""
-Triune Trainer
-
-This will gradually absorb the legacy train_llm.py.
-"""
+from .engine import TrainingEngine
 
 class Trainer:
+    """
+    High-level training interface.
 
-    def __init__(self, config):
+    This will gradually replace legacy/train_llm.py.
+    """
+
+    def __init__(
+        self,
+        *,
+        model,
+        optimizer,
+        train_loader,
+        eval_loader,
+        config,
+        device,
+    ):
+        self.model = model
+        self.optimizer = optimizer
+        self.train_loader = train_loader
+        self.eval_loader = eval_loader
         self.config = config
+        self.device = device
+
+        self.engine = TrainingEngine(self)
 
     def fit(self):
-        raise NotImplementedError("Training engine not migrated yet.")
+        return self.engine.train()
