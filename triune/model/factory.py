aaa3b51caf1251baf8135d38225321df58d4536e
Yash Bharadwaj <yashbharadwaj456@gmail.com>
2026-07-16 18:48:40 +0000

    Add model factory

---
+++ triune/model/factory.py
@@ -0,0 +1,8 @@
+from model import TriuneTransformer
+
+def build_model(config):
+    model = TriuneTransformer(
+        vocab_size=config["vocab_size"],
+        use_fp4=config["use_fp4"],
+    )
+    return model
